0acbfe1582c37973b300ad22a93e5cbfab7a938c
Ilya Tsymbal <ilya.tsymbal@gmail.com>
2022-01-24 16:37:24 -0500

    Added Float format

--- survey/src/main/java/com.quickbirdstudios.surveykit/backend/views/question_parts/FloatTextFieldPart.kt
+++ survey/src/main/java/com.quickbirdstudios.surveykit/backend/views/question_parts/FloatTextFieldPart.kt
@@ -2,6 +2,7 @@ package com.quickbirdstudios.surveykit.backend.views.question_parts
 
 import android.content.Context
 import android.text.InputType
+import android.text.method.DigitsKeyListener
 import android.view.View
 import androidx.annotation.StringRes
 import com.quickbirdstudios.surveykit.R
@@ -11,6 +12,7 @@ internal class FloatTextFieldPart(context: Context) : TextFieldPart(context) {
     init {
         id = R.id.floatFieldPartField
         this.field.inputType = InputType.TYPE_NUMBER_FLAG_DECIMAL
+        this.field.keyListener = (DigitsKeyListener.getInstance("1234567890."));
         this.textAlignment = View.TEXT_ALIGNMENT_CENTER
     }
 
